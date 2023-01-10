b0af60af2d36ece84086e70cc9d50babfa23b716
jamesjavanyc` <1931736857@qq.com>
2023-01-10 12:28:32 -0500

    init week 2 branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './header.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 
 @NgModule({
+  // 自定义组件
   declarations: [
     AppComponent,
     HeaderComponent,
     ContactListComponent,
     ContactDetailComponent
   ],
+  // 导入组件
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+// 组件装饰器 
 @Component({
+  // 组件选择器
   selector: 'app-header',
+  // html文件地址
   templateUrl: './header.component.html',
+  // 样式文件地址
   styleUrls: ['./header.component.css']
 })
+  
+  // 导出的组件类
 export class HeaderComponent {
 
 }
